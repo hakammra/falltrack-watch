4c4750ab72596dcce3c2bb6867220402bf10c513
Abdul Hakam <75803455+hakammra@users.noreply.github.com>
2026-04-13 00:45:31 +0530

    Add files via upload

---
+++ mobile_app/android/build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
